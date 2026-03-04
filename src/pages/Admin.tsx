3782b16d41c2bd43ba77ebbd0528a314b9073469
Jobulikannan <kannan@jobuli.com>
2026-03-04 16:13:28 +0530

    button updated
    04.03.2026

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Admin() {
+  const navigate = useNavigate();
   return (
     <div>
       <h2>Admin Dashboard</h2>
       <p>Only admin can see this page.</p>
+      <button onClick={() => navigate("/profile")}>Go to Profile</button>
     </div>
   );
 }
